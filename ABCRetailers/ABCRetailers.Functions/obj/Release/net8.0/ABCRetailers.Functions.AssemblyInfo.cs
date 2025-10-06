a4515fe39849d146d35ffc8425c5b50a18e4c6b3
unknown <st10452012imconnect.edu.za>
2025-10-06 23:27:02 +0200

    function update

--- ABCRetailers/ABCRetailers.Functions/obj/Release/net8.0/ABCRetailers.Functions.AssemblyInfo.cs
+++ ABCRetailers/ABCRetailers.Functions/obj/Release/net8.0/ABCRetailers.Functions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers.Functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5121fcab0c6065249285be0d7a2a8e50d32f5cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers.Functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers.Functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
